c22900b897bca72d4cad3e807e0161ac5e24fa82
count inn <countinn@gmail.com>
2016-06-03 22:57:28 -0300

    added more musical schemas toward song-updates....

--- routes/zs4mongoose.js
+++ routes/zs4mongoose.js
@@ -4,6 +4,10 @@ var mongoose = require('mongoose');
 var zs4 = require('./zs4utils');
 var zs4db = module.exports;
 
+var SCHEMA_PLAIN = 0;
+var SCHEMA_DATED = 1;
+var SCHEMA_DOCUMENT = (2|SCHEMA_DATED);
+
 zs4db.conn = mongoose.createConnection(process.env.ZS4_MONGODB_URL);;
 zs4db.conn.on('error', console.error.bind(console, 'connection error:'));
 zs4db.conn.once('open', function() {
@@ -13,11 +17,64 @@ zs4db.conn.once('open', function() {
 zs4db.schema = {};
 zs4db.model = {};
 
-zs4db.createSchema = function(object){
+zs4db.createSchema = function(object,mods){
+  if (mods&SCHEMA_DATED){
+    object.created = Date;
+    object.updated = Date;
+  }
+
+  if (mods&SCHEMA_DOCUMENT){
+    object.doc_owner = {
+      type: String,
+      required: true,
+      trim: true,
+      minlength:5,
+      maxlength:64,
+      index:true,
+    };
+    object.doc_status = {
+      type: String,
+      enum: ['d','p','t','x'], //draft, public, trash, x-terminated
+      required: true,
+    };
+    object.doc_title = {
+      type: String,
+      minlength:1,
+      maxlength:128,
+      required: true,
+      trim: true,
+    };
+  }
+
   var nu = new mongoose.Schema(object);
-  nu.methods.burps = function(){console.log("BURP!!!!!!!!!!!!!" + this);}
+
+  if (mods&SCHEMA_DATED){
+    nu.pre('save', function(next) {
+      // get the current date
+      var currentDate = new Date();
+
+      // change the updated_at field to current date
+      this.updated = currentDate;
+
+      // if created_at doesn't exist, add to that field
+      if (!this.created)
+        this.created = currentDate;
+
+      next();
+    });
+  }
+
+  if (mods&SCHEMA_DOCUMENT){
+    nu.post('init', function(next) {
+      // it's a DRAFT doc first!
+      this.doc_status = 'd';
+      this.doc_title = 'untitled';
+    });
+  }
+
   return nu;
 };
+
 
 /////////////////////////////////////////
 // identity schema
@@ -25,17 +82,17 @@ zs4db.createSchema = function(object){
 zs4db.schema.Identity = zs4db.createSchema({
     identity: { type: String, required: true, trim: true },
     provider: { type: String, required: true, trim: true },
-})
+},SCHEMA_PLAIN)
 
+/////////////////////////////////////////
 // User Record.
 zs4db.schema.User = zs4db.createSchema({
   name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true,  index: { unique: true }},
+  email: { type: String, required: true, trim: true, minlength:5, maxlength:64, index: { unique: true }},
   pic: { type: String, required: true, trim: true },
   auth:[zs4db.schema.Identity],
-});
+},SCHEMA_DATED);
 zs4db.model.User = zs4db.conn.model('User',zs4db.schema.User);
-
 
 zs4db.login = function(req,res){
 
@@ -56,7 +113,7 @@ zs4db.login = function(req,res){
          auth:[{identity:xu.identity,provider:xu.provider}],
        };
        var nu = zs4db.model.User(obj);
-	   
+
        nu.save(function(err) {
          console.log('inside save callback');
          if (err) {
@@ -88,11 +145,10 @@ zs4db.login = function(req,res){
           }
           console.log('User' + xu.email + ' updated!');
         });
-	
+
   });
 
 }
-
 zs4db.dumpAllUsers = function(){
   console.log('dumping all users:');
   zs4db.model.User.find({}, function(err, users) {
@@ -100,3 +156,47 @@ zs4db.dumpAllUsers = function(){
     console.log(users);
   });
 }
+
+/////////////////////////////////////////
+// Document
+
+
+/////////////////////////////////////////
+// Musical Schemas.
+
+zs4db.schema.Chord = zs4db.createSchema({
+  // chord root note
+  r: { type: Number, required: true, min: 0, max: 11 },
+  // chord type
+  t: { type: Number, required: true, min: 0, max: (2<<24) },
+  // bass note (indicates inversion)
+  b: { type: Number, required: true, min: 0, max: 11 },
+},SCHEMA_PLAIN);
+
+zs4db.schema.Event = zs4db.createSchema({
+  // Events can have chords
+  c: { type: zs4db.schema.Chord },
+  // timer event: measure, beats, ticks
+  t: { type: String, enum: ['m','b','t'] },
+  // (lead) voice note
+  v: { type: Number, min: 21, max: 108 },
+  // word (lyric)
+  w: { type: String, minlength: 1, maxlength: 32, trim: true },
+  // x-tras: stops, piano, forte, newline
+  x: { type: String, enum: ['s','p','f','n'] },
+},SCHEMA_PLAIN);
+
+zs4db.schema.Composition = zs4db.createSchema({
+  key: { type: Number, required: true, min: 0, max: 11 },
+  bpm: { type: Number, min: 30, max: 480, required: true },
+  tpb: { type: Number, min: 1, max: 10, required: true },
+  event:[zs4db.schema.Event],
+},SCHEMA_DOCUMENT);
+
+zs4db.schema.Composition.post('init', function(next) {
+  // it's a DRAFT doc first!
+  this.key = 0;
+  this.bpm = 120;
+  this.tpb = 3;
+});
+zs4db.model.Composition = zs4db.conn.model('Composition',zs4db.schema.Composition);
