70b40345c3279dc4c5685d3310d3f1d52eef9b44
count inn <countinn@gmail.com>
2016-09-22 11:39:47 +0200

    Bowser amendment

--- zs4/mongodb.js
+++ zs4/mongodb.js
@@ -105,7 +105,7 @@ mongodb.create = function(input){
 
   MONGODB._.property(new zs4.type.object({name:'config',flags:'',}))
   MONGODB.config._.property(new zs4.type.boolean({name:'connected',flags:'required noset nostore',default:false,}));
-  MONGODB.config._.property(new zs4.type.string({name:'url',flags:'required',default:'mongodb://127.0.0.1/'+dbname}));
+  MONGODB.config._.property(new zs4.type.string({name:'url',flags:'required',default:'mongodb://127.0.0.1/zs4'}));
 
   MONGODB.initializeArray = function(ARRAY){
 
