50e078a9c5c370b38cbdc2453f88fafdaf216299
Andy Flinn <countinn@gmail.com>
2017-12-25 07:18:05 +0100

    mongoose deprications eliminated

--- zs4/mongodb.js
+++ zs4/mongodb.js
@@ -299,7 +299,16 @@ mongodb.create = function(input){
     console.log('MONGODB CONNECT!');
 
     // Create the database connection
-    mongoose.connect(MONGODB.config.url._.value);
+    var options = {
+      useMongoClient: true,
+      autoIndex: false, // Don't build indexes
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+      reconnectInterval: 500, // Reconnect every 500ms
+      poolSize: 10, // Maintain up to 10 socket connections
+      // If not connected, return errors immediately rather than waiting for reconnect
+      bufferMaxEntries: 0
+    };
+    mongoose.connect(MONGODB.config.url._.value,options);
 
     // CONNECTION EVENTS
     // When successfully connected
