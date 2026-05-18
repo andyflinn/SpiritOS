8046bba33ce2c723b83e813a4ecef2ff0a3aa09a
Andy Flinn <countinn@gmail.com>
2026-05-18 02:30:45 +0200

    working on the typing system

--- packages/hxm/src/types/registry.js
+++ packages/hxm/src/types/registry.js
@@ -3,30 +3,35 @@
 const coreTypes = {
   // === Primitive Types ===
   "boolean": {
+    parenttype: null,
     name: "boolean",
     default: () => ({ _type: "boolean", value: false }),
     flags: { readonly: false }
   },
 
   "integer": {
+    parenttype: null,
     name: "integer",
     default: () => ({ _type: "integer", value: 0 }),
     flags: { readonly: false }
   },
 
   "float": {
+    parenttype: null,
     name: "float",
     default: () => ({ _type: "float", value: 0.0 }),
     flags: { readonly: false }
   },
 
   "string": {
+    parenttype: null,
     name: "string",
     default: () => ({ _type: "string", value: "" }),
     flags: { readonly: false }
   },
 
   "name": {
+    parenttype: "string",
     name: "name",
     default: () => ({ _type: "name", value: "" }),
     flags: { readonly: false }
@@ -34,12 +39,46 @@ const coreTypes = {
 
   // === Container Types ===
   "object": {
+    parenttype: null,
     name: "object",
+    members: {},
+    default: () => ({ _type: "object" }),
+    flags: { readonly: false }
+  },
+
+  "nametype": {
+    parenttype: "object",
+    name: "nametype",
+    members: {
+        name: { type: "name", required: true },
+        type: { type: "name", required: true }
+    },
+    default: () => ({ _type: "object" }),
+    flags: { readonly: false }
+  },
+
+  "transform": {
+    parenttype: "object",
+    name: "transform",
+    members: {
+        input: { type: "object", required: true },
+    },
+    default: () => ({ _type: "object" }),
+    flags: { readonly: false }
+  },
+
+ "createmember": {
+    parenttype: "transform",
+    name: "createmember",
+    members: {
+        input: { type: "nametype", required: true },
+    },
     default: () => ({ _type: "object" }),
     flags: { readonly: false }
   },
 
   "list": {
+    parenttype: "object",
     name: "list",
     default: () => ({
       _type: "list",
