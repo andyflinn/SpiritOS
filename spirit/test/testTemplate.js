84ecfd9c003c40fbac384ef3ef95be8e81b546b8
countinn <countinn@gmail.com>
2026-08-31 16:00:41 +0200

    deleted all the type manager stuff

--- spirit/test/testTemplate.js
+++ spirit/test/testTemplate.js
@@ -4,3 +4,10 @@ const spirit = require('../run/js/kernel.js');
 const test = require('./testSupport.js');
 
 test.startTest('Template Test');
+
+test.check('how to call a successful test unit');
+
+test.fail('how to log a failure');
+
+test.reportSuccessFailureCount();
+
